1ac2b25de0a08768a76aae21e05d3121a12a63db
jayakrishna2006 <147476150+jayakrishna2006@users.noreply.github.com>
2023-10-17 10:25:59 +0530

    #include<stdio.h> #include<math.h> int main() {   int a,b,c;   scanf("%d%d",&a,&b);   c=(a*b)/(a+b);   printf("%d",c); }

--- Water_Taps.c
+++ Water_Taps.c
@@ -1,8 +1,9 @@
 #include<stdio.h>
+#include<math.h>
 int main()
 {
-    int a,b,c;
-    scanf("%d%d",&a,&b);
-    c=a*b/(a+b);
-    printf("%d",c);
+  int a,b,c;
+  scanf("%d%d",&a,&b);  
+  c=(a*b)/(a+b);
+  printf("%d",c);
 }
