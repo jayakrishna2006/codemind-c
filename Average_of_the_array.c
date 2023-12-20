ac912fb741eaccee1720414d74730a81684e6d3f
jayakrishna2006 <147476150+jayakrishna2006@users.noreply.github.com>
2023-12-20 10:30:33 +0530

    #include<stdio.h> int main() {     int a,i;     scanf("%d",&a);     int arr[a];     for(i=0;i<a;i++)     {         scanf("%d",&arr[i]);     }     int sum=0;     for(i=0;i<a;i++){         sum+=arr[i];     }     printf("%.2f",(float)sum/a); }

---
+++ Average_of_the_array.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int a,i;
+    scanf("%d",&a);
+    int arr[a];
+    for(i=0;i<a;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    int sum=0;
+    for(i=0;i<a;i++){
+        sum+=arr[i];
+    }
+    printf("%.2f",(float)sum/a);
+}
